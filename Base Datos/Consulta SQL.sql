73d4d76a8c8643fa26efdc47cd28023a8fa427a9
jhasmany jhunnior fernandez ortega <fernandez.jhasmany@ficct.uagrm.edu.bo>
2021-10-24 21:07:11 -0400

    create file Consulta SQL.sql 20 %

--- Base Datos/Consulta SQL.sql
+++ Base Datos/Consulta SQL.sql
@@ -1,6 +1,12 @@
 USE CONCINITY;
 -- 1. Listar de administradores que se registraron el 25 de junio
--- 2. Listar de administradores que se registraron el 25 de junio
+-- 2. Listar  los  nombres  de  las  personas  que  hicieron  una  compra  el  25  de  octubre  del 2021
+-- 3. Listar  las  personas  que  no  tienen  CI
+-- 4. Listar las personas que hicieron más de 3 compras
+-- 5. Listar las personas que aun registran movimientos en la tienda
+-- 6. Listar  las  personas  que  aún  no  hicieron  pedidos  en  el  restaurante
+-- 7. Listar  las  facturas  con  monto  mayor  a  100  y  que  tengan  CI
+-- 8. Listar las personas que aún no hicieron compras en el tienda
 
 SELECT *FROM PERSONAL;
 SELECT *FROM ROL;
