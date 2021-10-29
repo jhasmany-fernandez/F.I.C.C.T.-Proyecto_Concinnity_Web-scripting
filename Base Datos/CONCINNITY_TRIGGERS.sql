332e3f5247d263ef67e224817e2fd9298f7c442d
jhasmany jhunnior fernandez ortega <fernandez.jhasmany@ficct.uagrm.edu.bo>
2021-10-29 02:39:32 -0400

    TERMINE DOCUMENTACION PARA LA PRIMERA PRESENTACION Y UPDATE TRIGGERS

--- Base Datos/CONCINNITY_TRIGGERS.sql
+++ Base Datos/CONCINNITY_TRIGGERS.sql
@@ -1,3 +1,4 @@
+-- Actualiza el stock, por cada inserción que se haga en el detalle de la nota de compra
 CREATE TRIGGER CANTIDAD_DECOMPRA_AL_STOCK
 ON DETALLE_NOTACOMPRA
 AFTER INSERT
@@ -12,9 +13,10 @@ AS
 	UPDATE PRODUCTO_TALLA SET STOCK=@stock WHERE ID_PRODUCTO=@id_producto AND ID_TALLA=@id_talla
 
 SELECT *FROM DETALLE_NOTACOMPRA;
-INSERT INTO DETALLE_NOTACOMPRA VALUES(1,1,7,50,8,400);
+INSERT INTO DETALLE_NOTACOMPRA VALUES(6,1,7,50,8,400);
 SELECT *FROM PRODUCTO_TALLA
 
+-- Actualiza el stock, por cada inserción que se haga en el detalle de la nota de venta
 CREATE TRIGGER ACTUALIZAR_MONTO_TOTAL
 ON DETALLE_NOTAVENTA
 AFTER INSERT
@@ -29,9 +31,15 @@ AS
 	SET @monto_total= @monto_pago-@monto_pago*@descuento
 	UPDATE NOTA_VENTA SET MONTO_TOTAL=@monto_total WHERE ID=@nvta
 
+INSERT INTO DETALLE_NOTAVENTA VALUES(1,1,7,70,1,70);
+INSERT INTO DETALLE_NOTAVENTA VALUES(1,2,1,200,1,200);
+
+
 SELECT *FROM DETALLE_NOTAVENTA;
 SELECT *FROM NOTA_VENTA;
 
+
+-- Actualiza el stock, por cada inserción que se haga en el detalle de la nota de salida
 CREATE TRIGGER ACTUALIZAR_MONTO_TOTAL_SALIDA
 ON DETALLE_NOTASALIDA
 AFTER INSERT
@@ -42,9 +50,18 @@ AS
 			@id_talla INT
 	SELECT  @id_producto=ID_PRODUCTO, @id_talla=ID_TALLA, @cant=CANTIDAD FROM INSERTED
 	SELECT @stock=STOCK FROM PRODUCTO_TALLA WHERE @id_producto=ID_PRODUCTO AND @id_talla=ID_TALLA
-	SET @stock=@stock+@cant
-	UPDATE PRODUCTO_TALLA SET STOCK=@stock WHERE ID_PRODUCTO=@id_producto AND ID_TALLA=@id_talla
+	if (@stock-@cant<0)
+	    BEGIN
+            ROLLBACK;
+        end
+	ELSE
+	    BEGIN
+            SET @stock=@stock-@cant
+	        UPDATE PRODUCTO_TALLA SET STOCK=@stock WHERE ID_PRODUCTO=@id_producto AND ID_TALLA=@id_talla
+        end
 
+
+INSERT INTO DETALLE_NOTASALIDA VALUES(1,2,2,7);
 
 SELECT *FROM DETALLE_NOTASALIDA;
 SELECT *FROM PRODUCTO_TALLA;
